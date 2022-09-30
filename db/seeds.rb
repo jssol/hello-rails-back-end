68c2ef9b05e178a3df9057ff906b56e84b11677b
Jonathan Sivahera <jsivahera@icloud.com>
2022-10-01 01:12:08 +0200

    Install Faker and seed db with quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Message.create(content: Faker::Quote.yoda)
+end
+
+10.times do
+  Message.create(content: Faker::Quote.famous_last_words)
+end
